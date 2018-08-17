3d96994b2f653c409db9f020f38fba07f181ef19
Justus Adam <dev@justus.science>
2018-08-17 16:48:45 +0200

    Wrote a bunch of new sections
    Pretty much deleted everything that was there before
    Also adding the notes

--- chapters/auto/stm-interface.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "stm-interface"
- (lambda ()
-   (LaTeX-add-labels
-    "sec:stm-interfaces"))
- :latex)
-
--- chapters/auto/stm-semantics.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "stm-semantics"
- (lambda ()
-   (LaTeX-add-labels
-    "sec:stm-semantics"))
- :latex)
-
